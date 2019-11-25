d97eaf7b918b7b1cfb5b7324af7b879ff15eaab9
Chovhan <dema200043@gmail.com>
2019-11-26 00:33:54 +0200

    Last labwork

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$('#button1').click(function() {
+  var ne = document.createElement("form");
+  ne.innerHTML = document.getElementsByClassName('smth1')[0].innerHTML;
+  document.body.appendChild(ne);
+})
+
+$('#button2').click(function() {
+  var ne = document.getElementsByTagName('form')[1];
+  document.body.removeChild(ne);
+})
+
+$('#button3').click(function() {
+  var ne = document.getElementsByTagName('input');
+  var jk = ne.length;
+  console.log(ne[i]);
+  for (var i=0;i<jk;i++) {
+    ne[i].value = "";
+  }
+})
+
+
+$(document).ready(function() {
+  var button = $("#for_audio");
+  var audio = new Audio('https://html5book.ru/examples/media/track.mp3');
+  setTimeout(function(){
+     audio.play();
+      }, 1);
+      button.click(function() {
+          audio.play();
+    });
+});
